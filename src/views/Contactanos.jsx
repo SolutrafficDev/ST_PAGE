bd4547aec88f712a08cd28df2a0f9d4a767842e2
vantwot <svaalentina123@gmail.com>
2026-05-13 20:39:18 -0500

    fix

--- src/views/Contactanos.jsx
+++ src/views/Contactanos.jsx
@@ -2,9 +2,11 @@ import { SecondaryText, BodyText } from "../components/Typography";
 
 const Contactanos = () => {
   return (
-    <section id="contacto" className="section">
-      <SecondaryText text="Contáctanos" />
-      <BodyText text="Email: contacto@empresa.com" />
+    <section id="contacto" className="section px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto">
+        <SecondaryText text="Contáctanos" />
+        <BodyText text="Email: contacto@empresa.com" />
+      </div>
     </section>
   );
 };
