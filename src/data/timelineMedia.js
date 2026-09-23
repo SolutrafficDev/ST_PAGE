f13588d99e182de3f971e9eee299e67691889478
vantwot <svaalentina123@gmail.com>
2026-09-23 14:24:27 -0500

    feat(sobre-nosotros): linea de tiempo con medios reales
    - Organiza src/assets/TIMELINE_ST en 19 hitos (portada + imagenes + videos)
    - Genera src/data/timelineMedia.js con los medios por hito
    - Carrusel vertical con galeria, lightbox ampliable y navegacion (flechas/teclado)
    - Quita el difuminado de los bordes del visor
    - Riel de puntos oculto en movil con contador de posicion

---
+++ src/data/timelineMedia.js
@@ -0,0 +1,251 @@
+// ARCHIVO GENERADO AUTOMATICAMENTE - no editar a mano.
+// Organiza los medios de la linea de tiempo por hito (src/assets/TIMELINE_ST).
+// Regenerar con: node scripts/timeline-index.mjs
+
+// 01_2015_Equipo_Cova
+import hito1Portada from "../assets/TIMELINE_ST/01_2015_Equipo_Cova/portada.jpg";
+import hito1_img_01_WhatsApp_Image_2026_09_23_at_1_01_28_PM from "../assets/TIMELINE_ST/01_2015_Equipo_Cova/img/01_WhatsApp_Image_2026-09-23_at_1_01_28_PM.jpg";
+import hito1_img_02_WhatsApp_Image_2026_09_23_at_12_16_34_PM from "../assets/TIMELINE_ST/01_2015_Equipo_Cova/img/02_WhatsApp_Image_2026-09-23_at_12_16_34_PM.jpg";
+import hito1_img_03_WhatsApp_Image_2026_09_23_at_1_01_28_PM_1 from "../assets/TIMELINE_ST/01_2015_Equipo_Cova/img/03_WhatsApp_Image_2026-09-23_at_1_01_28_PM_1.jpg";
+
+// 02_2018-03-20_Recepcion_ShowRoom
+import hito2Portada from "../assets/TIMELINE_ST/02_2018-03-20_Recepcion_ShowRoom/portada.jpg";
+import hito2_img_01_20180517_060629 from "../assets/TIMELINE_ST/02_2018-03-20_Recepcion_ShowRoom/img/01_20180517_060629.jpg";
+import hito2_img_02_20180517_060601 from "../assets/TIMELINE_ST/02_2018-03-20_Recepcion_ShowRoom/img/02_20180517_060601.jpg";
+import hito2_img_03_20180517_060633 from "../assets/TIMELINE_ST/02_2018-03-20_Recepcion_ShowRoom/img/03_20180517_060633.jpg";
+import hito2_vid_01_20180317_165952 from "../assets/TIMELINE_ST/02_2018-03-20_Recepcion_ShowRoom/video/01_20180317_165952.mp4";
+import hito2_vid_02_20180317_165857 from "../assets/TIMELINE_ST/02_2018-03-20_Recepcion_ShowRoom/video/02_20180317_165857.mp4";
+import hito2_vid_03_20180317_171533 from "../assets/TIMELINE_ST/02_2018-03-20_Recepcion_ShowRoom/video/03_20180317_171533.mp4";
+
+// 03_2018-05-04_Laboratorio_Produccion
+import hito3Portada from "../assets/TIMELINE_ST/03_2018-05-04_Laboratorio_Produccion/portada.jpg";
+import hito3_img_01_IMG_20180517_WA0031 from "../assets/TIMELINE_ST/03_2018-05-04_Laboratorio_Produccion/img/01_IMG-20180517-WA0031.jpg";
+import hito3_img_02_IMG_20180517_WA0033 from "../assets/TIMELINE_ST/03_2018-05-04_Laboratorio_Produccion/img/02_IMG-20180517-WA0033.jpg";
+import hito3_vid_01_20180418_095655 from "../assets/TIMELINE_ST/03_2018-05-04_Laboratorio_Produccion/video/01_20180418_095655.mp4";
+
+// 04_2018-05-30_Capacitacion_Personal
+import hito4Portada from "../assets/TIMELINE_ST/04_2018-05-30_Capacitacion_Personal/portada.jpg";
+import hito4_img_01_20180517_102859 from "../assets/TIMELINE_ST/04_2018-05-30_Capacitacion_Personal/img/01_20180517_102859.jpg";
+import hito4_img_02_20180517_102857 from "../assets/TIMELINE_ST/04_2018-05-30_Capacitacion_Personal/img/02_20180517_102857.jpg";
+import hito4_img_03_20180517_102850 from "../assets/TIMELINE_ST/04_2018-05-30_Capacitacion_Personal/img/03_20180517_102850.jpg";
+import hito4_vid_01_20180517_124349 from "../assets/TIMELINE_ST/04_2018-05-30_Capacitacion_Personal/video/01_20180517_124349.mp4";
+import hito4_vid_02_20180517_124715 from "../assets/TIMELINE_ST/04_2018-05-30_Capacitacion_Personal/video/02_20180517_124715.mp4";
+import hito4_vid_03_20180517_124046 from "../assets/TIMELINE_ST/04_2018-05-30_Capacitacion_Personal/video/03_20180517_124046.mp4";
+import hito4_vid_04_20180517_124312 from "../assets/TIMELINE_ST/04_2018-05-30_Capacitacion_Personal/video/04_20180517_124312.mp4";
+
+// 05_2018-06-19_Mundial_Solutraffic
+import hito5Portada from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/portada.jpg";
+import hito5_img_01_IMG_20180619_WA0048 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/01_IMG-20180619-WA0048.jpg";
+import hito5_img_02_IMG_20180619_WA0157 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/02_IMG-20180619-WA0157.jpg";
+import hito5_img_03_IMG_20180619_WA0159 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/03_IMG-20180619-WA0159.jpg";
+import hito5_img_04_IMG_20180619_WA0169 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/04_IMG-20180619-WA0169.jpg";
+import hito5_img_05_IMG_20180619_WA0154 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/05_IMG-20180619-WA0154.jpg";
+import hito5_img_06_IMG_20180619_WA0049 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/06_IMG-20180619-WA0049.jpg";
+import hito5_img_07_IMG_20180619_WA0160 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/07_IMG-20180619-WA0160.jpg";
+import hito5_img_08_IMG_20180619_WA0168 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/08_IMG-20180619-WA0168.jpg";
+import hito5_img_09_IMG_20180619_WA0053 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/09_IMG-20180619-WA0053.jpg";
+import hito5_img_10_IMG_20180619_WA0052 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/10_IMG-20180619-WA0052.jpg";
+import hito5_img_11_IMG_20180619_WA0050 from "../assets/TIMELINE_ST/05_2018-06-19_Mundial_Solutraffic/img/11_IMG-20180619-WA0050.jpg";
+
+// 06_2018-06-21_Inicio_Central_Palmira
+import hito6Portada from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/portada.jpg";
+import hito6_img_01_20180621_123222 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/01_20180621_123222.jpg";
+import hito6_img_02_20180622_103718 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/02_20180622_103718.jpg";
+import hito6_img_03_20180621_123935 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/03_20180621_123935.jpg";
+import hito6_img_04_20180621_123946 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/04_20180621_123946.jpg";
+import hito6_img_05_20180621_123228 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/05_20180621_123228.jpg";
+import hito6_img_06_20180621_123239 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/06_20180621_123239.jpg";
+import hito6_img_07_20180621_123219 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/07_20180621_123219.jpg";
+import hito6_img_08_20180621_123920 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/08_20180621_123920.jpg";
+import hito6_img_09_20180621_123237 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/09_20180621_123237.jpg";
+import hito6_img_10_20180622_103726 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/10_20180622_103726.jpg";
+import hito6_img_11_20180621_123250 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/11_20180621_123250.jpg";
+import hito6_img_12_20180621_123244 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/12_20180621_123244.jpg";
+import hito6_img_13_20180621_123217 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/13_20180621_123217.jpg";
+import hito6_img_14_20180621_123907 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/14_20180621_123907.jpg";
+import hito6_img_15_20180621_124005 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/15_20180621_124005.jpg";
+import hito6_img_16_20180621_123940 from "../assets/TIMELINE_ST/06_2018-06-21_Inicio_Central_Palmira/img/16_20180621_123940.jpg";
+
+// 07_2018-12-21_Solutraffic_Humana
+import hito7Portada from "../assets/TIMELINE_ST/07_2018-12-21_Solutraffic_Humana/portada.jpg";
+import hito7_img_01_IMG_20181221_WA0047 from "../assets/TIMELINE_ST/07_2018-12-21_Solutraffic_Humana/img/01_IMG-20181221-WA0047.jpg";
+import hito7_img_02_IMG_20181221_WA0046 from "../assets/TIMELINE_ST/07_2018-12-21_Solutraffic_Humana/img/02_IMG-20181221-WA0046.jpg";
+import hito7_img_03_IMG_20181221_WA0045 from "../assets/TIMELINE_ST/07_2018-12-21_Solutraffic_Humana/img/03_IMG-20181221-WA0045.jpg";
+import hito7_img_04_IMG_20181221_WA0044 from "../assets/TIMELINE_ST/07_2018-12-21_Solutraffic_Humana/img/04_IMG-20181221-WA0044.jpg";
+
+// 08_2019-02-28_Entrega_Central_Palmira
+import hito8Portada from "../assets/TIMELINE_ST/08_2019-02-28_Entrega_Central_Palmira/portada.jpg";
+import hito8_img_01_IMG_20190228_WA0051 from "../assets/TIMELINE_ST/08_2019-02-28_Entrega_Central_Palmira/img/01_IMG-20190228-WA0051.jpg";
+import hito8_img_02_IMG_20190228_WA0056 from "../assets/TIMELINE_ST/08_2019-02-28_Entrega_Central_Palmira/img/02_IMG-20190228-WA0056.jpg";
+import hito8_img_03_IMG_20190228_WA0059 from "../assets/TIMELINE_ST/08_2019-02-28_Entrega_Central_Palmira/img/03_IMG-20190228-WA0059.jpg";
+import hito8_img_04_IMG_20190228_WA0050 from "../assets/TIMELINE_ST/08_2019-02-28_Entrega_Central_Palmira/img/04_IMG-20190228-WA0050.jpg";
+import hito8_img_05_IMG_20190228_WA0057 from "../assets/TIMELINE_ST/08_2019-02-28_Entrega_Central_Palmira/img/05_IMG-20190228-WA0057.jpg";
+import hito8_img_06_IMG_20190228_WA0060 from "../assets/TIMELINE_ST/08_2019-02-28_Entrega_Central_Palmira/img/06_IMG-20190228-WA0060.jpg";
+import hito8_img_07_20190724_115257 from "../assets/TIMELINE_ST/08_2019-02-28_Entrega_Central_Palmira/img/07_20190724_115257.jpg";
+import hito8_img_08_IMG_20190228_WA0061 from "../assets/TIMELINE_ST/08_2019-02-28_Entrega_Central_Palmira/img/08_IMG-20190228-WA0061.jpg";
+
+// 09_2019-03-12_Controlador_Cova_Inalambrico
+import hito9Portada from "../assets/TIMELINE_ST/09_2019-03-12_Controlador_Cova_Inalambrico/portada.jpg";
+import hito9_img_01_IMG_20190312_WA0018 from "../assets/TIMELINE_ST/09_2019-03-12_Controlador_Cova_Inalambrico/img/01_IMG-20190312-WA0018.jpg";
+import hito9_img_02_IMG_20190312_WA0080 from "../assets/TIMELINE_ST/09_2019-03-12_Controlador_Cova_Inalambrico/img/02_IMG-20190312-WA0080.jpg";
+import hito9_img_03_IMG_20190312_WA0016 from "../assets/TIMELINE_ST/09_2019-03-12_Controlador_Cova_Inalambrico/img/03_IMG-20190312-WA0016.jpg";
+import hito9_img_04_IMG_20190312_WA0019 from "../assets/TIMELINE_ST/09_2019-03-12_Controlador_Cova_Inalambrico/img/04_IMG-20190312-WA0019.jpg";
+
+// 10_2019-07-22_Prototipo_MTix
+import hito10Portada from "../assets/TIMELINE_ST/10_2019-07-22_Prototipo_MTix/portada.jpg";
+import hito10_img_01_20190722_094320 from "../assets/TIMELINE_ST/10_2019-07-22_Prototipo_MTix/img/01_20190722_094320.jpg";
+import hito10_img_02_20190722_094340 from "../assets/TIMELINE_ST/10_2019-07-22_Prototipo_MTix/img/02_20190722_094340.jpg";
+import hito10_img_03_20190722_094326 from "../assets/TIMELINE_ST/10_2019-07-22_Prototipo_MTix/img/03_20190722_094326.jpg";
+
+// 11_2019-10-03_Capacitacion_Palmira
+import hito11Portada from "../assets/TIMELINE_ST/11_2019-10-03_Capacitacion_Palmira/portada.jpg";
+import hito11_img_01_20191003_125340 from "../assets/TIMELINE_ST/11_2019-10-03_Capacitacion_Palmira/img/01_20191003_125340.jpg";
+import hito11_img_02_20191003_130208 from "../assets/TIMELINE_ST/11_2019-10-03_Capacitacion_Palmira/img/02_20191003_130208.jpg";
+import hito11_img_03_20191003_130219 from "../assets/TIMELINE_ST/11_2019-10-03_Capacitacion_Palmira/img/03_20191003_130219.jpg";
+import hito11_img_04_20191003_125353 from "../assets/TIMELINE_ST/11_2019-10-03_Capacitacion_Palmira/img/04_20191003_125353.jpg";
+import hito11_img_05_20191003_125339 from "../assets/TIMELINE_ST/11_2019-10-03_Capacitacion_Palmira/img/05_20191003_125339.jpg";
+import hito11_vid_01_20191003_122839 from "../assets/TIMELINE_ST/11_2019-10-03_Capacitacion_Palmira/video/01_20191003_122839.mp4";
+
+// 12_2019-10-24_Ensamble_Prototipo_MTix
+import hito12Portada from "../assets/TIMELINE_ST/12_2019-10-24_Ensamble_Prototipo_MTix/portada.jpg";
+import hito12_img_01_Screenshot_20240215_112503_WhatsApp from "../assets/TIMELINE_ST/12_2019-10-24_Ensamble_Prototipo_MTix/img/01_Screenshot_20240215_112503_WhatsApp.jpg";
+import hito12_img_02_20191004_062924 from "../assets/TIMELINE_ST/12_2019-10-24_Ensamble_Prototipo_MTix/img/02_20191004_062924.jpg";
+import hito12_img_03_Screenshot_20240215_112429_WhatsApp from "../assets/TIMELINE_ST/12_2019-10-24_Ensamble_Prototipo_MTix/img/03_Screenshot_20240215_112429_WhatsApp.jpg";
+
+// 13_2024-12-18_Mantenimiento_Jamundi
+import hito13Portada from "../assets/TIMELINE_ST/13_2024-12-18_Mantenimiento_Jamundi/portada.jpg";
+import hito13_img_01_13_Diciembre_18_2024 from "../assets/TIMELINE_ST/13_2024-12-18_Mantenimiento_Jamundi/img/01_13_Diciembre_18_2024.jpg";
+
+// 14_2024-12-26_Entrega_Body_Cams_Candelaria
+import hito14Portada from "../assets/TIMELINE_ST/14_2024-12-26_Entrega_Body_Cams_Candelaria/portada.jpg";
+import hito14_vid_01_14_Diciembre_26_2024 from "../assets/TIMELINE_ST/14_2024-12-26_Entrega_Body_Cams_Candelaria/video/01_14_Diciembre_26_2024.mp4";
+
+// 15_2025-07-17_Produccion_Girardot
+import hito15Portada from "../assets/TIMELINE_ST/15_2025-07-17_Produccion_Girardot/portada.jpg";
+import hito15_img_01_20250717_135211 from "../assets/TIMELINE_ST/15_2025-07-17_Produccion_Girardot/img/01_20250717_135211.jpg";
+import hito15_img_02_20250711_060636 from "../assets/TIMELINE_ST/15_2025-07-17_Produccion_Girardot/img/02_20250711_060636.jpg";
+
+// 16_2025-11-26_Produccion_Santa_Marta
+import hito16Portada from "../assets/TIMELINE_ST/16_2025-11-26_Produccion_Santa_Marta/portada.jpg";
+import hito16_img_01_20251126_070614 from "../assets/TIMELINE_ST/16_2025-11-26_Produccion_Santa_Marta/img/01_20251126_070614.jpg";
+import hito16_img_02_20251126_094131 from "../assets/TIMELINE_ST/16_2025-11-26_Produccion_Santa_Marta/img/02_20251126_094131.jpg";
+
+// 17_2026-02-08_Vehiculo_Analitica_Estadio
+import hito17Portada from "../assets/TIMELINE_ST/17_2026-02-08_Vehiculo_Analitica_Estadio/portada.jpg";
+import hito17_img_01_20260208_105041 from "../assets/TIMELINE_ST/17_2026-02-08_Vehiculo_Analitica_Estadio/img/01_20260208_105041.jpg";
+import hito17_img_02_20260208_105052 from "../assets/TIMELINE_ST/17_2026-02-08_Vehiculo_Analitica_Estadio/img/02_20260208_105052.jpg";
+import hito17_img_03_20260208_105043 from "../assets/TIMELINE_ST/17_2026-02-08_Vehiculo_Analitica_Estadio/img/03_20260208_105043.jpg";
+import hito17_vid_01_20260208_105300_01 from "../assets/TIMELINE_ST/17_2026-02-08_Vehiculo_Analitica_Estadio/video/01_20260208_105300_01.mp4";
+import hito17_vid_02_20260208_105414 from "../assets/TIMELINE_ST/17_2026-02-08_Vehiculo_Analitica_Estadio/video/02_20260208_105414.mp4";
+
+// 18_2026-03-15_Remodelacion_Laboratorio
+import hito18Portada from "../assets/TIMELINE_ST/18_2026-03-15_Remodelacion_Laboratorio/portada.jpg";
+import hito18_img_01_WhatsApp_Image_2026_09_23_at_1_59_22_PM_1 from "../assets/TIMELINE_ST/18_2026-03-15_Remodelacion_Laboratorio/img/01_WhatsApp_Image_2026-09-23_at_1_59_22_PM_1.jpg";
+import hito18_img_02_WhatsApp_Image_2026_09_23_at_1_59_22_PM from "../assets/TIMELINE_ST/18_2026-03-15_Remodelacion_Laboratorio/img/02_WhatsApp_Image_2026-09-23_at_1_59_22_PM.jpg";
+
+// 19_2026-05-26_Controladores_Solares_Tauramena
+import hito19Portada from "../assets/TIMELINE_ST/19_2026-05-26_Controladores_Solares_Tauramena/portada.jpg";
+import hito19_img_01_WhatsApp_Image_2026_09_03_at_3_37_23_PM from "../assets/TIMELINE_ST/19_2026-05-26_Controladores_Solares_Tauramena/img/01_WhatsApp_Image_2026-09-03_at_3_37_23_PM.jpg";
+import hito19_img_02_WhatsApp_Image_2026_09_03_at_3_37_24_PM_1 from "../assets/TIMELINE_ST/19_2026-05-26_Controladores_Solares_Tauramena/img/02_WhatsApp_Image_2026-09-03_at_3_37_24_PM_1.jpg";
+import hito19_img_03_20260526_084903 from "../assets/TIMELINE_ST/19_2026-05-26_Controladores_Solares_Tauramena/img/03_20260526_084903.jpg";
+
+
+export const timelineMedia = [
+  {
+    portada: hito1Portada,
+    imagenes: [hito1_img_01_WhatsApp_Image_2026_09_23_at_1_01_28_PM, hito1_img_02_WhatsApp_Image_2026_09_23_at_12_16_34_PM, hito1_img_03_WhatsApp_Image_2026_09_23_at_1_01_28_PM_1],
+    videos: [],
+  },
+  {
+    portada: hito2Portada,
+    imagenes: [hito2_img_01_20180517_060629, hito2_img_02_20180517_060601, hito2_img_03_20180517_060633],
+    videos: [hito2_vid_01_20180317_165952, hito2_vid_02_20180317_165857, hito2_vid_03_20180317_171533],
+  },
+  {
+    portada: hito3Portada,
+    imagenes: [hito3_img_01_IMG_20180517_WA0031, hito3_img_02_IMG_20180517_WA0033],
+    videos: [hito3_vid_01_20180418_095655],
+  },
+  {
+    portada: hito4Portada,
+    imagenes: [hito4_img_01_20180517_102859, hito4_img_02_20180517_102857, hito4_img_03_20180517_102850],
+    videos: [hito4_vid_01_20180517_124349, hito4_vid_02_20180517_124715, hito4_vid_03_20180517_124046, hito4_vid_04_20180517_124312],
+  },
+  {
+    portada: hito5Portada,
+    imagenes: [hito5_img_01_IMG_20180619_WA0048, hito5_img_02_IMG_20180619_WA0157, hito5_img_03_IMG_20180619_WA0159, hito5_img_04_IMG_20180619_WA0169, hito5_img_05_IMG_20180619_WA0154, hito5_img_06_IMG_20180619_WA0049, hito5_img_07_IMG_20180619_WA0160, hito5_img_08_IMG_20180619_WA0168, hito5_img_09_IMG_20180619_WA0053, hito5_img_10_IMG_20180619_WA0052, hito5_img_11_IMG_20180619_WA0050],
+    videos: [],
+  },
+  {
+    portada: hito6Portada,
+    imagenes: [hito6_img_01_20180621_123222, hito6_img_02_20180622_103718, hito6_img_03_20180621_123935, hito6_img_04_20180621_123946, hito6_img_05_20180621_123228, hito6_img_06_20180621_123239, hito6_img_07_20180621_123219, hito6_img_08_20180621_123920, hito6_img_09_20180621_123237, hito6_img_10_20180622_103726, hito6_img_11_20180621_123250, hito6_img_12_20180621_123244, hito6_img_13_20180621_123217, hito6_img_14_20180621_123907, hito6_img_15_20180621_124005, hito6_img_16_20180621_123940],
+    videos: [],
+  },
+  {
+    portada: hito7Portada,
+    imagenes: [hito7_img_01_IMG_20181221_WA0047, hito7_img_02_IMG_20181221_WA0046, hito7_img_03_IMG_20181221_WA0045, hito7_img_04_IMG_20181221_WA0044],
+    videos: [],
+  },
+  {
+    portada: hito8Portada,
+    imagenes: [hito8_img_01_IMG_20190228_WA0051, hito8_img_02_IMG_20190228_WA0056, hito8_img_03_IMG_20190228_WA0059, hito8_img_04_IMG_20190228_WA0050, hito8_img_05_IMG_20190228_WA0057, hito8_img_06_IMG_20190228_WA0060, hito8_img_07_20190724_115257, hito8_img_08_IMG_20190228_WA0061],
+    videos: [],
+  },
+  {
+    portada: hito9Portada,
+    imagenes: [hito9_img_01_IMG_20190312_WA0018, hito9_img_02_IMG_20190312_WA0080, hito9_img_03_IMG_20190312_WA0016, hito9_img_04_IMG_20190312_WA0019],
+    videos: [],
+  },
+  {
+    portada: hito10Portada,
+    imagenes: [hito10_img_01_20190722_094320, hito10_img_02_20190722_094340, hito10_img_03_20190722_094326],
+    videos: [],
+  },
+  {
+    portada: hito11Portada,
+    imagenes: [hito11_img_01_20191003_125340, hito11_img_02_20191003_130208, hito11_img_03_20191003_130219, hito11_img_04_20191003_125353, hito11_img_05_20191003_125339],
+    videos: [hito11_vid_01_20191003_122839],
+  },
+  {
+    portada: hito12Portada,
+    imagenes: [hito12_img_01_Screenshot_20240215_112503_WhatsApp, hito12_img_02_20191004_062924, hito12_img_03_Screenshot_20240215_112429_WhatsApp],
+    videos: [],
+  },
+  {
+    portada: hito13Portada,
+    imagenes: [hito13_img_01_13_Diciembre_18_2024],
+    videos: [],
+  },
+  {
+    portada: hito14Portada,
+    imagenes: [],
+    videos: [hito14_vid_01_14_Diciembre_26_2024],
+  },
+  {
+    portada: hito15Portada,
+    imagenes: [hito15_img_01_20250717_135211, hito15_img_02_20250711_060636],
+    videos: [],
+  },
+  {
+    portada: hito16Portada,
+    imagenes: [hito16_img_01_20251126_070614, hito16_img_02_20251126_094131],
+    videos: [],
+  },
+  {
+    portada: hito17Portada,
+    imagenes: [hito17_img_01_20260208_105041, hito17_img_02_20260208_105052, hito17_img_03_20260208_105043],
+    videos: [hito17_vid_01_20260208_105300_01, hito17_vid_02_20260208_105414],
+  },
+  {
+    portada: hito18Portada,
+    imagenes: [hito18_img_01_WhatsApp_Image_2026_09_23_at_1_59_22_PM_1, hito18_img_02_WhatsApp_Image_2026_09_23_at_1_59_22_PM],
+    videos: [],
+  },
+  {
+    portada: hito19Portada,
+    imagenes: [hito19_img_01_WhatsApp_Image_2026_09_03_at_3_37_23_PM, hito19_img_02_WhatsApp_Image_2026_09_03_at_3_37_24_PM_1, hito19_img_03_20260526_084903],
+    videos: [],
+  },
+];
+
+export default timelineMedia;
